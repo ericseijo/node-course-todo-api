faacd8d55880d25947f64f5bfb9964de4d9a4e40
Eric Seijo <ericseijo@gmail.com>
2017-09-05 21:51:32 -0400

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -16,17 +16,23 @@ const users = [{
 }, {
   _id: userTwoId,
   email: 'eric2@example.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId.toHexString(), access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'FIrst test todo'
+  text: 'FIrst test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 333
+  completedAt: 333,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
